36eefcee805f46eee37865ff2ed8da0d4bfd6559
an244 <thienan.le244@gmail.com>
2017-10-13 11:51:32 +0700

    xong toan bo phan login

--- public/xuly.js
+++ public/xuly.js
@@ -6,18 +6,31 @@ $(document).ready(()=>{
 
     $('#btnLogin').click(()=>{       
         const userNameFromLogin = $('#txtUserName').val();
+
+        //Bước 1.1: từ browser gửi user từ textbox lên server
         socket.emit('Client-send-UserName',userNameFromLogin);
         $('#txtUserName').val('');    
     });
 
-
+//Bước 1.2.2: sau khi server kiểm tra và phát hiện trùng user thì gửi về client để báo người dùng đổi tên khác.
 socket.on('Server-send-Duplicate-user-Login-Fail',(userNameFromLogin)=>{
     alert(userNameFromLogin +' is existed already. Choose another one!');
 });
 
-socket.on('Server-send-Login-successful',(newUser)=>{
+//Bước 1.2.4: client lắng nghe sau khi server đã thêm phần tử vô mảng
+//ẩn form login, mở form chat và hiện Hello tên người login
+socket.on('Server-send-Login-successful',(newUserFromLogin)=>{
     $('#loginForm').hide();
     $('#chatForm').show();
-    $('#currentUser').html(newUser);
-});    
+    $('#currentUser').html(newUserFromLogin);
+}); 
+
+//Bước 2.2: client nhận mảng usersArray từ server để hiện thị trong List User Online
+socket.on('Server-send-list-username-online', (usersArrayFromServer)=>{
+    $('#namesOnline').html('')//xoa noi dung trc ghi gan user online;
+    usersArrayFromServer.forEach((name)=>{
+        $('#namesOnline').append("<div class='usersOnline'>"+name+ "</div>");
+    });
+
+});
 });
--- server.js
+++ server.js
@@ -21,15 +21,28 @@ io.on("connection", socket =>{
     console.log(socket.id + ' : Disconnected!');
   });
 
+  //Bước 1.2: Server nhận user từ client gửi lên. Sau đó kiểm tra tên trong mảng 
   socket.on('Client-send-UserName', (userNameFromLogin)=>{
+    //kiểm tra sự tồn tại user trong mảng
     const isExist = usersArray.some(user =>{
       return user === userNameFromLogin;
     });
 
+    //Bước 1.2.1: Nếu đã tồn tại user thì đáp trả về cho client biết chọn user khác
     if(isExist) return socket.emit('Server-send-Duplicate-user-Login-Fail',userNameFromLogin);
+    
+    //Nếu ko trùng thì thêm phần tử vô mảng userArray
     usersArray.push(userNameFromLogin);
+    socket.userNameSocket = userNameFromLogin; //tự tạo biến userNameSocket để phân biệt các socketid bằng tên.
+    
+    //Bước 1.2.3: sau khi thêm phần tử vô mảng userArray thì server sẽ gửi thông báo về client để tạo newUserFromLogin
     socket.emit('Server-send-Login-successful', userNameFromLogin);
+
+    //Bước 2.1: server gửi mảng userArray cho client để hiện trong phần List User Online
+    io.sockets.emit('Server-send-list-username-online',(usersArray));
+
   });
+
 });
 
 app.get('/',(req,res)=> {
